791c903f029c9f43d1e15c62e9ae2b510adca3d3
thjiang <browserify@qq.com>
2017-04-29 15:01:08 +0800

    性能优化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-default/index.css';
+// import ElementUI from 'element-ui';
+import { Button, Select, Table, TableColumn, Collapse, CollapseItem, Form, FormItem,
+     Col, Input, Radio, RadioGroup, RadioButton, Checkbox, CheckboxGroup, Loading, Message } from 'element-ui';
+// import 'element-ui/lib/theme-default/index.css';
 import App from './app.vue';
 
 Vue.config.debug = true;
 
-Vue.use(ElementUI);
+Vue.use(Button);
+Vue.use(Select);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Col);
+Vue.use(Input);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(RadioButton);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+
+Vue.use(Loading.directive);
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$message = Message;
+
 Vue.use(VueResource);
 
 new Vue(App);
